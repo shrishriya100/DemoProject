76c6d35257a0983c3b9aa5da793dcd918e275175
shriya <shrishriya001@gmail.com>
2021-05-18 22:48:55 +0530

    Add function added

---
+++ Test/src/com/shigi/test/AddNumbers.java
@@ -0,0 +1,10 @@
+package com.shigi.test;
+
+public class AddNumbers {
+
+	public double sumOfNumber(double x, double y) {
+		return x+y;
+	}
+	
+
+}
--- Test/src/com/shigi/test/Demo.java
+++ Test/src/com/shigi/test/Demo.java
@@ -4,6 +4,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		AddNumbers addXY = new AddNumbers();
+		addXY.sumOfNumber(10, 20);
+		addXY.sumOfNumber(10.5, 200.67);
+
 
 	}
 
